0fcad234ee7cee2411b8c4770a03c8007fd1b450
rsstdd <rosstod@gmail.com>
2017-01-15 13:46:56 -0800

    Trnsfered front

--- app/app.js
+++ app/app.js
@@ -1,3 +1,67 @@
-import angular from 'angular'
+import angular from 'angular';
 
-angular.module('my-app', []);
+import uiRouter from 'angular-ui-router'
+
+import angularMaterialize from 'angular-materialize'
+
+// import AuthCtrl from './authentication/auth.controller'
+// import AuthSvc from './authentication/auth.service'
+import CartCtrl from './cart/cart.controller'
+import CatalogCtrl from './catalog/catalog.controller'
+
+import CartService from './cart/cart.service'
+import CatalogService from './catalog/catalog.service'
+
+angular.module('snapShopApp', [angularMaterialize, uiRouter])
+  // .controller('AuthCtrl', AuthCtrl)
+  // .service('AuthSvc', AuthSvc)
+  .service('CatalogService', CatalogService)
+  .service('CartService', CartService)
+
+  .controller('CatalogCtrl', CatalogCtrl)
+  .controller('CartCtrl', CartCtrl)
+
+  .config(['$stateProvider', ($stateProvider) => {
+    $stateProvider
+      .state('home', {
+        url: '',
+        templateUrl: 'views/home.html',
+        controller: 'CatalogCtrl',
+        controllerAs: 'catalogCtrl'
+      })
+      .state('main', {
+        url: '/',
+        templateUrl: 'views/home.html',
+        controller: 'CatalogCtrl',
+        controllerAs: 'catalogCtrl',
+        title: 'Snap Shop'
+      })
+      .state('cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl',
+        controllerAs: 'cartCtrl',
+        title: 'Cart'
+      })
+      .state('checkout', {
+        url: '/checkout',
+        templateUrl: 'views/checkout.html',
+        controller: 'CartCtrl',
+        controllerAs: 'cartCtrl',
+        title: 'Checkout'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'views/auth.html',
+        title: 'Sign in',
+        controller: 'AuthCtrl as authCtrl',
+        controllerAs: 'authCtrl'
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: '/views/auth.html',
+        title: 'Sign up',
+        controller: 'AuthCtrl',
+        controllerAs: 'authCtrl'
+      });
+  }]);
--- app/catalog/catalog.controller.js
+++ app/catalog/catalog.controller.js
@@ -1,18 +1,19 @@
 export default  class CatalogCtrl {
-    constructor(catalogSvc) {
-      this.searchText = '';
-      this.catalogSvc = catalogSvc;
-      catalogSvc.getCameras()
-        .then((cameras) => {
-          this.cameras = cameras;
-        })
-        .catch((err) => console.log(err));
-    }
+  constructor(catalogSvc) {
+    this.searchText = '';
+    this.catalogSvc = catalogSvc;
+    catalogSvc.getCameras()
+      .then((cameras) => {
+        this.cameras = cameras;
+      })
+      .catch((err) => console.log(err));
+  }
 
-    getCameras() {
-      console.log(cameras);
-      return this.catalogSvc.cameras;
-    }
+  getCameras() {
+    console.log(cameras);
+
+    return this.catalogSvc.cameras;
+  }
 }
 
 CatalogCtrl.$inject = ['CatalogService'];
